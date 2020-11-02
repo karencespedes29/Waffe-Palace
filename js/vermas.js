0f5e04fa41d4d2c8c7f8d26b1aefd6ab4b615d7a
karencespedes29 <73839997+karencespedes29@users.noreply.github.com>
2020-11-02 12:59:34 -0500

    Add files via upload

---
+++ js/vermas.js
@@ -0,0 +1,9 @@
+ function Mostra(){
+            $(".hola").show();
+            $(".andy").hide();
+        }
+        function btnocultar(){
+            $(".hola").hide();
+            $(".andy").show();
+        }
+
